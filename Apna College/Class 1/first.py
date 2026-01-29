b0c2d10ea56afe8ff4cc14f2ca7946c931468331
maliksiddiq <maliksiddiqawan21@gmail.com>
2026-01-29 12:11:21 -0900

    Data Types Done!

---
+++ Apna College/Class 1/first.py
@@ -0,0 +1,110 @@
+# print('Hello World!')
+# print('My name is Siddiq')
+# print('I am a Student!')
+# print('Hello World' , 'My name is Siddiq' , 'I am Student!')
+# print('Commit')
+
+# ---------------------------------------------------------------------------
+
+# name = 'Siddiq'
+# age = 20
+# shirtPrice = 22.5
+
+# print(name)
+# print(age)
+# print(shirtPrice)
+
+# print('My name is : ' , name)
+# print('I am ' , age , ' Years Old')
+# print('My shirt price is ' , shirtPrice)
+
+# print(name + age)   Its a Wrong Way to code in python!
+
+# name = 'Malik Siddiq Awan!'
+# age = 20
+# age1 = age
+# print(name)
+# print('I am ' , age , 'Years Old')
+# print('I am ' , age1 , 'Years Old')
+
+# ---------------------------------------------------------------------------
+
+# Identifier Rules   >>>> Identifier wo name hote hai jo kisi variable ko ya kisi class ya object ya function ko hum dete hai
+
+# 1 >>>  Start With Alphabet  >>> a to z     -----   A to Z   and special character Underscore _
+# 2 >>>  We cant use special Character but only Undersore _
+# 3 >>>  No start With number
+# 4 >>> No use python Keywords
+
+# firstValue = 300
+# second2Value = 600
+# _thirdValue = 900
+# fourth_value = 1200
+
+# print(firstValue)
+# print(second2Value)
+# print(_thirdValue)
+# print(fourth_value)
+
+# ---------------------------------------------------------------------------
+
+# Data Types in python 
+
+# 1 integer =  +20  ,  -20   ,  0
+# 2 Float =  2.5   ,  +2.5    , -3.5
+# 3 str = 'Ali'   , "Ali"  , "'Ali"
+# 4 Boolean  =  << True   ====  <<  False
+# 5 None  =   None
+
+
+# name1 = 'Malik'
+# name2 = "Malik"
+# name3 = "'Malik'"
+
+# print(type(name1))
+# print(type(name2))
+# print(type(name3))
+
+# ---------------------------------
+
+# num1  = +25
+# num2 = -25
+# num3 = 0
+
+# print(type(num1))
+# print(type(num2))
+# print(type(num3))
+
+
+# ---------------------------------
+
+# _num1 = -2.5
+# _num2 = +2.5
+# _num3 = 0.00
+
+# print(type(_num1))
+# print(type(_num2))
+# print(type(_num3))
+
+# ---------------------------------
+
+# isTrue = True
+# isFalse = False
+
+# print(type(isTrue))
+# print(type(isFalse))
+
+# ---------------------------------
+
+# emptyValue  = None
+# print(type(emptyValue))
+
+# ---------------------------------------------------------------------------
+
+# Keywords!    >>> keywords kabhi bhi identifier ka name nhi ho skte 
+
+# Keywords are reserved words in python   >>>   is  , in  , True  , False , if , return , while , lambda , try , with  , None
+
+
+# ---------------------------------------------------------------------------
+
